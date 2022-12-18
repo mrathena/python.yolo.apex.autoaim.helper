aa9880c887662ba3044ea91d91a53d84cd4fa1d3
mrathena <china.mrathena@gmail.com>
2022-12-18 23:19:27 +0800

    .

---
+++ detect.realtime.py
@@ -0,0 +1,24 @@
+import time
+
+import cv2
+from win32con import HWND_TOPMOST, SWP_NOMOVE, SWP_NOSIZE
+from win32gui import FindWindow, SetWindowPos
+
+from toolkit import Detector
+
+region = (3440 // 5 * 2, 1440 // 3, 3440 // 5, 1440 // 3)
+weight = 'weights.apex.group.967082372.5.6.1.s.640.10000.engine'
+detector = Detector(weight)
+
+title = 'Realtime ScreenGrab Detect'
+while True:
+
+    _, img = detector.detect(region=region, image=True, label=True, confidence=True)
+    cv2.namedWindow(title, cv2.WINDOW_AUTOSIZE)
+    cv2.imshow(title, img)
+    SetWindowPos(FindWindow(None, title), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE)
+    t3 = time.time()
+    k = cv2.waitKey(1)  # 0:不自动销毁也不会更新, 1:1ms延迟销毁
+    if k % 256 == 27:
+        cv2.destroyAllWindows()
+        exit('ESC ...')
